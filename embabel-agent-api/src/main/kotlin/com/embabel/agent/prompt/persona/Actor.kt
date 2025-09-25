0602658a3785bb32ccfb9c8373027dd66c2efd59
Rod Johnson <johnsonroda@gmail.com>
2025-09-25 15:29:28 -0700

    Add Actor

---
+++ embabel-agent-api/src/main/kotlin/com/embabel/agent/prompt/persona/Actor.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024-2025 Embabel Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.embabel.agent.prompt.persona
+
+import com.embabel.common.ai.model.LlmOptions
+import com.embabel.common.ai.prompt.PromptContributor
+
+/**
+ * An Actor plays a role: Convenient way to combine a PromptContributor
+ * with an LLM.
+ */
+data class Actor<T : PromptContributor>(
+    val persona: T,
+    val llm: LlmOptions,
+)
